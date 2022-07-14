07bd5734bbd001abe28106feb2c0f44de17aad57
Dicki Syafrudin <dicki.syaf@gmail.com>
2022-07-15 04:31:26 +0700

    Task D5 Yeayyy

---
+++ assets/javascript/Blog.js
@@ -0,0 +1,122 @@
+let blogs = []
+
+let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+function addBlog(event) {
+    event.preventDefault()
+
+    let title = document.getElementById("input-blog-title").value
+    let content = document.getElementById("input-blog-content").value
+    let image = document.getElementById("input-blog-image")
+
+    image = URL.createObjectURL(image.files[0])
+
+
+    let blog = {
+        title,
+        content,
+        image,
+        postedAt: new Date()
+    }
+
+    blogs.push(blog)
+    renderBlogs()
+}
+
+function renderBlogs() {
+    console.log(blogs);
+
+    let containerBlogs = document.getElementById("contents")
+
+    containerBlogs.innerHTML = ""
+
+    for (let i = 0; i < blogs.length; i++) {
+        containerBlogs.innerHTML += `
+        <div class="blog-list-item">
+            <div class="blog-image">
+                <img src="${blogs[i].image}" alt="" />
+            </div>
+            <div class="blog-content">
+                <h1>
+                    <a href="Blog Detail.html" target="_blank">${blogs[i].title}</a>
+                </h1>
+                
+                <div class="detail-blog-content">
+                    ${getFullTime(blogs[i].postedAt)} | Dicki Syafrudin
+                </div>
+                
+                <p>
+                    ${blogs[i].content}
+                </p>
+                <div style="text-align: right;">
+                    <span style="font-size: 15px; color: grey;">${getDistanceTime(blogs[i].postedAt)}</span>
+                </div>
+                
+                <div class="btn-group">
+                    <button class="btn-edit">Edit Post</button>
+                    <button class="btn-post">Post Blog</button>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+
+function getFullTime(time) {
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+
+    let hour = time.getHours()
+    let minute = time.getMinutes()
+
+    return `${date} ${month[monthIndex]} ${year} ${hour}:${minute} WIB`
+}
+
+function getDistanceTime(time) {
+    // waktu posting = 08.30 => time
+    // waktu aktual = 08.35 => new Date()
+    // sudah berapa lama?
+    // waktu aktual - waktu posting
+
+    let distance = new Date() - new Date(time)
+
+    let miliseconds = 1000
+    let secondInMinutes = 59
+    let minuteInHour = 59
+    let secondInHour = secondInMinutes * minuteInHour // 3600
+    let hourInDay = 23
+
+    let dayDistance = distance / (miliseconds * secondInHour * hourInDay)
+
+    if (dayDistance >= 1) {
+        const dayDate = Math.floor(dayDistance) + ' day ago'
+        return dayDate
+    } else {
+        let hourDistance = Math.floor(distance / (miliseconds * secondInHour))
+        if (hourDistance > 0) {
+            return hourDistance + ' hour ago'
+        } else {
+            let minuteDistance = Math.floor(distance / (miliseconds * secondInMinutes))
+            return minuteDistance + ' minute ago'
+        }
+    }
+
+}
+
+setInterval(function () {
+    renderBlogs()
+}, 2000)
